36355007c76d24cb397bf7d9f851e97494c99ab5
Charles Hoffman <centipedefarmer@gmail.com>
2014-01-11 20:26:42 -0600

    fix the directory paths a bit

--- bin/status_statter.rb
+++ bin/status_statter.rb
@@ -1,7 +1,10 @@
 #!/usr/bin/env ruby
-$:.unshift(File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib')))
+dir = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+$:.unshift(File.join(dir, 'lib'))
 require 'status_statter'
-Dir['status_statter/trackers/total/*.rb'].each {|file| require file }
+Dir[File.join(dir, 'lib/status_statter/trackers/*.rb')].each do |file|
+  require file
+end
 statter = StatusStatter.new
 statter.register Total
 statter.run
